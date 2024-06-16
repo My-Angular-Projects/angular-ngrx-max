19595dac97c1327351a0b63d9489654778d319e4
Valery Semenenko <gearmobile@gmail.com>
2024-06-16 23:00:15 +0300

    add devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { counterReducer } from '@store/reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(),
     provideState({ name: 'counter', reducer: counterReducer }),
-  ],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
